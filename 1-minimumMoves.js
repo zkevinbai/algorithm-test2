8a8ff990aae168da8ba793ae3dfb76bd9cd9eef9
Kevin Bai <lkevin957@gmail.com>
2019-04-02 15:42:31 -0700

    Debug minor issue, complete minimum moves

---
+++ 1-minimumMoves.js
@@ -0,0 +1,35 @@
+// input
+    // 2 arrays with 2 values each
+// output
+    // incrementing or decrementing by 1 at a time, how many moves are needed
+    // such that the first array becomes equal to the second one
+    // return total number of moves
+
+
+// let a = [
+//     1234,
+//     4321
+// ];
+
+// let m = [
+//     2345,
+//     3214
+// ];
+
+function minimumMoves(a, m) {
+    let totalMoves = 0;
+
+    let aString = a.join("");
+    let mString = m.join("");
+
+    for (let i = 0; i < aString.length; i++) {
+        const aNumber = parseInt(aString[i]);
+        const mNumber = parseInt(mString[i]);
+        
+        totalMoves += Math.abs( aNumber - mNumber );
+    }
+
+   return totalMoves;
+}
+
+// minimumMoves(a, m);
