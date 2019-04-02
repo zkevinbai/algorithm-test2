368a798b9a55802e912473dbfb46ef3f464adcb2
Kevin Bai <lkevin957@gmail.com>
2019-04-02 15:57:21 -0700

    Add basic function to test expected outcome

--- 2-remoteCities.js
+++ 2-remoteCities.js
@@ -1,6 +1,22 @@
 // Input
-    // 1: Starting Node
+    // 1: Number of Nodes
     // 2: All the Froms
     // 3: All the Tos
 // Output
     // a distance for each city (the starting node, and every fromTo pair);
+
+
+let gNodes = 4;
+let gFrom = [1, 4, 4, 1];
+let gTo = [3, 3, 2, 2];
+
+let stack`
+
+function findRemoteness(gNodes, gFrom, gTo) {
+    let zeros = [];
+    for (let i = 0; i < gNodes; i++){
+        zeros.push(0)
+    };
+
+    return zeros;
+};
